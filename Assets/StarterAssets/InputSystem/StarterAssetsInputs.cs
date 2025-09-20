8a28c32622c02a7256d7d70a9871165659643246
Jr <125832010+JrMontoya490@users.noreply.github.com>
2025-09-19 22:23:37 -0600

    update

---
+++ Assets/StarterAssets/InputSystem/StarterAssetsInputs.cs
@@ -0,0 +1,102 @@
+using UnityEngine;
+#if ENABLE_INPUT_SYSTEM 
+using UnityEngine.InputSystem;
+#endif
+
+namespace StarterAssets
+{
+	public class StarterAssetsInputs : MonoBehaviour
+	{
+		[Header("Character Input Values")]
+		public Vector2 move;
+		public Vector2 look;
+		public bool jump;
+		public bool sprint;
+		
+		// Variable para agacharse
+		public bool crouch;
+
+		[Header("Movement Settings")]
+		public bool analogMovement;
+
+#if !UNITY_IOS || !UNITY_ANDROID
+		[Header("Mouse Cursor Settings")]
+		public bool cursorLocked = true;
+		public bool cursorInputForLook = true;
+#endif
+
+#if ENABLE_INPUT_SYSTEM 
+		public void OnMove(InputValue value)
+		{
+			MoveInput(value.Get<Vector2>());
+		}
+
+		public void OnLook(InputValue value)
+		{
+			if(cursorInputForLook)
+			{
+				LookInput(value.Get<Vector2>());
+			}
+		}
+
+		public void OnJump(InputValue value)
+		{
+			JumpInput(value.isPressed);
+		}
+
+		public void OnSprint(InputValue value)
+		{
+			SprintInput(value.isPressed);
+		}
+		
+		// --- CÓDIGO CORREGIDO: Método para la acción de agacharse ---
+		public void OnCrouch(InputValue value)
+		{
+			if (value.isPressed)
+			{
+				CrouchInput(!crouch); // Cambia el estado actual (de true a false o viceversa)
+			}
+		}
+		// -----------------------------------------------------------------
+#endif
+
+
+		public void MoveInput(Vector2 newMoveDirection)
+		{
+			move = newMoveDirection;
+		}
+
+		public void LookInput(Vector2 newLookDirection)
+		{
+			look = newLookDirection;
+		}
+
+		public void JumpInput(bool newJumpState)
+		{
+			jump = newJumpState;
+		}
+
+		public void SprintInput(bool newSprintState)
+		{
+			sprint = newSprintState;
+		}
+
+		// Método para manejar el estado de agacharse
+		public void CrouchInput(bool newCrouchState)
+		{
+			crouch = newCrouchState;
+		}
+		
+#if !UNITY_IOS || !UNITY_ANDROID
+		private void OnApplicationFocus(bool hasFocus)
+		{
+			SetCursorState(cursorLocked);
+		}
+
+		private void SetCursorState(bool newState)
+		{
+			Cursor.lockState = newState ? CursorLockMode.Locked : CursorLockMode.None;
+		}
+#endif
+	}
+}
